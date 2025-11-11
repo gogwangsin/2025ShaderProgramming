657bd1c0b14f773d77866ca7c69cb2b87ab79081
gogwangsin <gwangsin1029@naver.com>
2025-11-11 13:23:52 +0900

    가로 막대기 여러개 출력

--- SimpleGame/Shaders/FS.glsl
+++ SimpleGame/Shaders/FS.glsl
@@ -5,18 +5,17 @@ layout(location=0) out vec4 FragColor;
 in vec2 v_UV;
 // RS 지나고하면 도형이 차지하는 화면의 픽셀만큼 들어온다.
 
+const float c_PI = 3.141592;
+
 void main()
 {
 	vec4 newColor = vec4(0);
+	
+//	float xValue = sin(v_UV.x * 2 * c_PI * 4);
+	float xValue = pow(abs(sin(v_UV.x * 2 * c_PI * 4)), 10); // 0.5도 해볼것
 
-	if(v_UV.x > 0.5)
-	{
-		newColor.r = 1;
-	}
-	else
-	{
-		newColor.g = 1;
-	}
+	newColor = vec4(vec3(xValue), 1);
+	
 
 	FragColor = newColor;
 }
